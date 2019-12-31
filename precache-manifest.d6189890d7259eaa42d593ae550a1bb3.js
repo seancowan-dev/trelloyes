81dd46b3e5e960504dda63ee42429792344d865a
Sean <sean.cowan1@gmail.com>
2019-12-30 23:31:59 -0500

    Updates

---
+++ precache-manifest.d6189890d7259eaa42d593ae550a1bb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af5d16fe85a889e3c9ae2cc0a3eb324",
+    "url": "/trelloyes/index.html"
+  },
+  {
+    "revision": "e7a8f7113d2bf784ec35",
+    "url": "/trelloyes/static/css/main.9ebf3b2f.chunk.css"
+  },
+  {
+    "revision": "5c07e35bf759ed1d88c9",
+    "url": "/trelloyes/static/js/2.4dff308c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/trelloyes/static/js/2.4dff308c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7a8f7113d2bf784ec35",
+    "url": "/trelloyes/static/js/main.06c5de7a.chunk.js"
+  },
+  {
+    "revision": "cdf916409ba4ff50e4f6",
+    "url": "/trelloyes/static/js/runtime-main.2a543cf8.js"
+  }
+]);
